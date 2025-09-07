3f0c249f07305eb3d4e6b3ad3df3ab3d4e3dd4bd
Diego Arnoldo Azuela Rosas <diego.azuela@gmail.com>
2025-09-06 23:13:15 -0700

    fix: rearranging project struct

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-import requests
-from constants import *
-from utils import build_url
-
-def read_api_key(path):
-    with open(path, "r") as f:
-        return f.read().strip()
+from alpha_vantage_client import AlphaVantageClient
 
 if __name__=="__main__":
-    # Gather information for the request
     ticker = input("Enter the stock ticker symbol: ").upper()
-    api_key = read_api_key("apikey.txt")
-    url = build_url(FUNCTION_INCOME_STATEMENT, ticker, api_key)
-    # Run request
-    response = requests.get(url)
-    if response.ok:
-        data = response.json()
-        print(data)
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
+    client = AlphaVantageClient("apikey.txt")
+    
+    data = client.get_income_statement(ticker)
+    print(data)
